fa51ad31bf45057011d634701ee1c7f01a358f2c
Brad R. <48097311+BradRollins@users.noreply.github.com>
2025-10-21 18:21:44 -0400

    Updated Gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Card } from "@/components/ui/card";
 import gallery1 from "@/assets/joint_meetup.jpg";
-import gallery2 from "@/assets/montgomery_center";
+import gallery2 from "@/assets/montgomery_center.jpg";
 import gallery3 from "@/assets/friday_coffee.jpg";
 
 const Gallery = () => {
